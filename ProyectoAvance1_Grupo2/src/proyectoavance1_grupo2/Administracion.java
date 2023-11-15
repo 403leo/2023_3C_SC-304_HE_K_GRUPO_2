b98585c89379f870d6f371737427a72dbbd53e0b
Josue Valverde <jvalverde40358@ufide.ac.cr>
2023-11-14 18:10:37 -0600

    Prueba 1

--- ProyectoAvance1_Grupo2/src/proyectoavance1_grupo2/Administracion.java
+++ ProyectoAvance1_Grupo2/src/proyectoavance1_grupo2/Administracion.java
@@ -2,15 +2,18 @@ package proyectoavance1_grupo2;
 
 public class Administracion {
 
+    //Atributos
     private int FilasDiscapacitadas;
     private int FilasNoDiscapacitadas;
     private int NumerosAsientosDiscapacitados;
     private int NumerosAsientosNoDiscapacitados;
     private String nombreEmpresa;
-    private double costoKilometraje;
+    private double costoKilometraje = 100;
 
     public void Kilometraje(double costoKilometraje) {
 
+        
+        
     } // Final del metodo kilometraje
 
 } // Final de la clase Administracion
--- ProyectoAvance1_Grupo2/src/proyectoavance1_grupo2/Estacion.java
+++ ProyectoAvance1_Grupo2/src/proyectoavance1_grupo2/Estacion.java
@@ -2,13 +2,25 @@ package proyectoavance1_grupo2;
 
 public class Estacion {
 
+    //Atributos
     private String nombre;
     private Cola colaDiscapacitada;
     private Cola colaNoDiscapacitada;
     
-    //
+    //Metodo encolarPasajero
+    public void encolarPasajero(Pasajero pasajero){
+        
+        
+        
+    }//Final de encolarPasajero
     
+    //Metodo desencolarPasajero
+    public void desencolarPasajero(Pasajero pasajero){
+            
+        
+        
+    }//Final de desencolarPasajero
+        
     
-    //
 
 } // Final de la clase Estacion
